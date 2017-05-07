db3af8345defe4860497c82da1fa28776cf314b0
Dídac Sabatés <sabatesduran@gmail.com>
2017-05-07 02:30:38 +0200

    Remove jQuery

--- js/index.js
+++ js/index.js
@@ -1,13 +1,23 @@
-// Write text for custom data
-$.getJSON('data/data.json', function(data) {
-    add_text_to_id("name", data.name);
-    add_text_to_id("job", data.job);
-    create_social_links(data.social_links);
+// Get data file
+fetch('data/data.json').then(function(response) {
+  if(response.ok) {
+    return response.json();
+  }
+  throw new Error('Network response was not ok.');
+}).then(function(data) {
+  add_text_to_id("name", data.name);
+  add_text_to_id("job", data.job);
+  create_social_links(data.social_links);
 });
 
-// Create github cards
-$.getJSON('https://api.github.com/users/sabatesduran/repos', function(data) {
-  $(data).each(function(index, repo) {
+// Get Github API data
+fetch('https://api.github.com/users/sabatesduran/repos').then(function(response) {
+  if(response.ok) {
+    return response.json();
+  }
+  throw new Error('Network response was not ok.');
+}).then(function(data) {
+  data.forEach(function(repo) {
     if(!repo.fork) {
       create_github_card(repo);
     }

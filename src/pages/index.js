0f6cdc18c52b5b947815c821df78e633aac56764
Dídac Sabatés <sabatesduran@gmail.com>
2018-02-14 19:43:51 +0100

    Update front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,33 @@ import Link from "gatsby-link";
 
 const IndexPage = () => (
   <div>
-    <h1>Dídac Sabatés</h1>
-    <p>Welcome to my site.</p>
+    <h1>Hi,</h1>
+    <p>
+      My name is Dídac, I'm a software developer from Barcelona, currenly living
+      in Amsterdam.
+    </p>
+
+    <h4>Things i like:</h4>
+    <ul>
+      <li>Coffee ☕</li>
+      <li>Making things with my hands 🖐️</li>
+      <li>Teaching 📚</li>
+      <li>Learning new stuff 📚📚</li>
+    </ul>
+
+    <h4>Tech I normally/currently use: </h4>
+    <ul>
+      <li>Linux &hearts;</li>
+      <li>VSCode</li>
+      <li>Terminator</li>
+      <li>Git</li>
+      <li>ES6</li>
+      <li>React</li>
+      <li>Ruby on Rails</li>
+      <li>GraphQL</li>
+      <li>Boostrap 4</li>
+      <li>Fira Code (This actual font)</li>
+    </ul>
   </div>
 );
 

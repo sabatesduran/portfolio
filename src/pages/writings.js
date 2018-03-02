ff7b7559912b74cd148d16c6eeb3ed5b007e4d81
Dídac Sabatés <sabatesduran@gmail.com>
2018-03-02 20:13:58 +0100

    Add dev.to badge in Writings page

--- src/pages/writings.js
+++ src/pages/writings.js
@@ -4,7 +4,17 @@ import ArticleCard from "../components/ArticleCard";
 
 const Writings = ({ data }) => (
   <div>
-    <h1>Writings</h1>
+    <div className="d-flex align-items-center">
+      <h1 className="">Writings</h1> &nbsp;
+      <a className="d-flex" href="https://dev.to/sabatesduran">
+        <img
+          src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg"
+          alt="Dídac Sabatés's DEV Profile"
+          height="25"
+          width="25"
+        />
+      </a>
+    </div>
     <div className="writings">
       {data.allWritingsJson.edges.map(article => (
         <ArticleCard key={article.node.title} article={article} />

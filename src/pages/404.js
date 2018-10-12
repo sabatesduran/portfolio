791b1a99ea4ec2ed6c09c395f57074df0da1b162
Dídac Sabatés <sabatesduran@gmail.com>
2018-10-12 17:56:58 +0200

    New 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,28 @@
 import React from "react";
+import LostLightsOn from "../images/lost_lights_on.webp";
+import LostLightsOff from "../images/lost_lights_off.webp";
 
 const NotFoundPage = () => (
   <div>
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div
+      style={{
+        width: "100%",
+        display: "flex"
+      }}
+    >
+      <img
+        src={
+          JSON.parse(localStorage.getItem("sabatesduran-config-v0")).darkMode
+            ? LostLightsOff
+            : LostLightsOn
+        }
+        style={{
+          width: "70%",
+          height: "100%"
+        }}
+      />
+    </div>
   </div>
 );
 
